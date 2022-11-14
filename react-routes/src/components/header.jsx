5e3a44edc9430c87746722f7ffa26ce5ada77df6
Nevin Alisheva <nevin.alisheva@gmail.com>
2022-11-14 16:31:52 +0100

    third checkpoint

--- react-routes/src/components/header.jsx
+++ react-routes/src/components/header.jsx
@@ -2,13 +2,16 @@ import { NavLink } from "react-router-dom";
 import classes from "./header.module.css";
 
 export const Header = () => {
+    const handleActiveLink = ({ isActive }) => {
+        return isActive ? { color: "yellow" } : null;
+      };
  return (
    <header className={classes.header}>
      <i> site logo </i>
      <nav className={classes.nav}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/dogs">Dogs</NavLink>
-        <NavLink to="/cats">Cats</NavLink>
+        <NavLink style={handleActiveLink} to="/">Home</NavLink>
+        <NavLink style={handleActiveLink} to="/dogs">Dogs</NavLink>
+        <NavLink style={handleActiveLink} to="/cats">Cats</NavLink>
      </nav>
    </header>
  );
