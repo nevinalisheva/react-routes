dc1cf18a9b118eb2611ed60edecf9bc011b825d5
Nevin Alisheva <nevin.alisheva@gmail.com>
2022-11-14 16:43:16 +0100

    fifth checkpoint

--- react-routes/src/App.js
+++ react-routes/src/App.js
@@ -4,6 +4,7 @@ import { Header } from "./components/header";
 import { Home } from "./pages/home";
 import { Dogs } from "./pages/dogs";
 import { Cats } from "./pages/cats";
+import { DogsBreedsPage } from './pages/dogs/dogs-breed';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/cats" element={<Cats/>} />
           <Route path="/dogs" element={<Dogs/>} />
+          <Route path="/dogs/:id" element={<DogsBreedsPage />} />
         </Routes>
       </Router>
     </div>
