d4fadd3721c613eac4c97e4b9346059036b4fe53
Nevin Alisheva <nevin.alisheva@gmail.com>
2022-11-14 16:28:49 +0100

    second checkpoint

--- react-routes/src/App.js
+++ react-routes/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./components/header";
 import { Home } from "./pages/home";
 import { Dogs } from "./pages/dogs";
 import { Cats } from "./pages/cats";
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/cats" element={<Cats/>} />
